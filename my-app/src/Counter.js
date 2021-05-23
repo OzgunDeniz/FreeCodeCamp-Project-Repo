387613e79a83b0f1a0e6aa168ec2cc00cac72531
Özgün Deniz <ozgundenz@gmail.com>
2021-05-23 23:33:39 +0300

    problem couldnot solved.

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -5,12 +5,14 @@ class Counter extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        count: 0
+        count: 0 ,
+        totalNumber : 0
       };
       // Change code below this line
       this.increment = this.increment.bind(this);
       this.decrement = this.decrement.bind(this);
       this.reset = this.reset.bind(this);
+      this.total = this.total.bind(this);
       // Change code above this line
     }
     // Change code below this line
@@ -35,6 +37,25 @@ class Counter extends Component {
         count: 0
       }));
     }
+    // assuming this.state = { value: 0 };
+     // this.setState((state) => ({ value: state.value + 1}));
+    //  buySlice() {
+    //   const totalSlices = this.state.slices === 20 ;
+    //   this.setState({
+    //     slices: totalSlices +1 
+    //   });
+    
+
+    total (){
+        if(this.state.count === 0 ){
+        const totalNumbers = this.state.totalNumber +1 
+        this.setState ({
+          totalNumber : totalNumbers
+        });
+     }
+    }
+
+
     // Change code above this line
     render() {
       return (
@@ -42,7 +63,7 @@ class Counter extends Component {
           <button className='inc' onClick={this.increment}>Increment!</button>
           <button className='dec' onClick={this.decrement}>Decrement!</button>
           <button className='reset' onClick={this.reset}>Reset</button>
-         <h1>Current Count: {this.state.count}</h1>
+         <h1>Current Count: {this.state.count} , {this.state.totalNumber}</h1>
         </div>
       );
     }
